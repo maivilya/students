cee73fd71533d9fd78a787a7d296c5742e1347cc
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-14 12:57:56 +0500

    added getStudent into the StudentController

--- src/main/java/ru/kontur/students/StudentController.java
+++ src/main/java/ru/kontur/students/StudentController.java
@@ -3,6 +3,7 @@ package ru.kontur.students;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController()
 public class StudentController {
@@ -17,5 +18,10 @@ public class StudentController {
     @GetMapping("/student/{id}")
     public Student getStudent(@PathVariable int id) {
         return studentService.getStudent(id);
+    }
+
+    @GetMapping("/student")
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 }
