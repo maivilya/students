bcf3581e6cf172e2b0b3330885fd02ef83a92f13
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-14 12:18:12 +0500

    added class Student

---
+++ src/main/java/ru/kontur/students/Student.java
@@ -0,0 +1,22 @@
+package ru.kontur.students;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private static int currentId;
+    private final int id;
+    private final String name;
+    private String groupName;
+
+    static {
+        currentId = 1;
+    }
+
+    public Student(String name, String groupName) {
+        id = currentId++;
+        this.name = name;
+        this.groupName = groupName;
+    }
+}
