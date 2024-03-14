d909c10605c7b84c24069d326af41e14926e831f
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-14 13:15:36 +0500

    added getStudent by name into the StudentController

--- src/main/java/ru/kontur/students/StudentController.java
+++ src/main/java/ru/kontur/students/StudentController.java
@@ -2,6 +2,7 @@ package ru.kontur.students;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -23,5 +24,10 @@ public class StudentController {
     @GetMapping("/student")
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @GetMapping("/student/search")
+    public List<Student> getStudents(@RequestParam String studentName) {
+        return studentService.getStudentsByName(studentName);
     }
 }
