ba665ca7e6a88a3dae79b729f7557e31d419db86
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-17 20:16:05 +0500

    added constructor and basic initialization

--- src/main/java/ru/kontur/students/Repository/StudentRepository.java
+++ src/main/java/ru/kontur/students/Repository/StudentRepository.java
@@ -1,4 +1,30 @@
 package ru.kontur.students.Repository;
 
+import org.springframework.stereotype.Repository;
+import ru.kontur.students.Entity.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class StudentRepository {
+
+    private final List<Student> students;
+
+    public StudentRepository() {
+        this.students = new ArrayList<>();
+        initialization();
+    }
+
+    private void initialization() {
+        students.add(new Student("Илья", "A1"));
+        students.add(new Student("Нияз", "A1"));
+        students.add(new Student("Евгений", "B2"));
+        students.add(new Student("Алексей", "A1"));
+        students.add(new Student("Кристина", "C1"));
+        students.add(new Student("Максим", "C1"));
+        students.add(new Student("Артем", "A1"));
+        students.add(new Student("Илья", "C3"));
+        students.add(new Student("Евгений", "C3"));
+    }
 }
