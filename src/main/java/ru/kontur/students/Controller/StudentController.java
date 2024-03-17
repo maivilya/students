9ee640d895d85c750593831daa7b0e090f63af1a
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-17 19:11:26 +0500

    move StudentController to the Controller package

--- src/main/java/ru/kontur/students/Controller/StudentController.java
+++ src/main/java/ru/kontur/students/Controller/StudentController.java
@@ -1,9 +1,12 @@
-package ru.kontur.students;
+package ru.kontur.students.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.kontur.students.Entity.Student;
+import ru.kontur.students.Service.StudentService;
+
 import java.util.List;
 
 @RestController()
