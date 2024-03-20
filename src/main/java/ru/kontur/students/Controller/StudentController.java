6b57d3dae8d8ed10d3923b6aa33cbcd474a6ee27
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-03-20 19:24:22 +0500

    added user method into the StudentController

--- src/main/java/ru/kontur/students/Controller/StudentController.java
+++ src/main/java/ru/kontur/students/Controller/StudentController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.kontur.students.Service.StudentService;
 
@@ -22,5 +23,11 @@ public class StudentController {
     public String users(Model model) {
         model.addAttribute("studentTable", studentService.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("{id}")
+    public String user(@PathVariable int id, Model model) {
+        model.addAttribute("student",studentService.getStudentById(id));
+        return "studentProfile";
     }
 }
