d9aaf07ca4dcece096465641a19c220b37e03dff
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-04-01 22:49:22 +0500

    make from Student to Entity

--- src/main/java/ru/kontur/students/Entity/Student.java
+++ src/main/java/ru/kontur/students/Entity/Student.java
@@ -1,17 +1,26 @@
 package ru.kontur.students.Entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="students")
 public class Student {
 
     private static int currentId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String groupName;
 
     static {
         currentId = 1;
+    }
+
+    public Student() {
+
     }
 
     public Student(String name, String groupName) {
