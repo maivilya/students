0310dbe36553c57cc57e00daaa5647e02a675e35
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2024-05-03 19:14:06 +0500

    added SecurityConfiguration

---
+++ src/main/java/ru/kontur/students/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package ru.kontur.students.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("user/**").hasAnyAuthority("user", "admin")
+                        .requestMatchers("admin/**").hasAuthority("admin")
+                        .requestMatchers("auth/**").authenticated()
+                        .requestMatchers("any/**").permitAll()
+                        .anyRequest().denyAll()
+                )
+                .build();
+    }
+}
